245170260b8f59b6b59dad23982d8cdac873c46d
toine <toine.schnabel@gmail.com>
2018-09-24 12:53:30 +0200

    Initial Commit

---
+++ volgMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  volgMe
+//
+//  Created by Toine Schnabel on 24-09-18.
+//  Copyright © 2018 Toine Schnabel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
